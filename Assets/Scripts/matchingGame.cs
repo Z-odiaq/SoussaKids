015412130754439873bc060f6552fca74e4f9be5
Mohamed Ammar <mohamedammar10@live.fr>
2022-04-24 23:47:26 +0100

    bug fixes & cleanup

--- Assets/Scripts/matchingGame.cs
+++ Assets/Scripts/matchingGame.cs
@@ -10,17 +10,10 @@ public class matchingGame : MonoBehaviour
     public bool isDrawing = false;
     public Camera cam;
     public GameObject endPoint;
-    public Vector2 endPos;
-    // public new Collider2D collider;
-    // Start is called before the first frame update
+
+
     void Start()
     {
-        // lineRenderer = GetComponent<LineRenderer>();
-        // endPoint = GetComponent<GameObject>();
-        // collider = GetComponent<Collider2D>();
-        // canvas = GetComponent<Canvas>();
-        Debug.Log("started");
-        //  canvas.renderMode = RenderMode.WorldSpace;
 
     }
 
@@ -32,10 +25,7 @@ public class matchingGame : MonoBehaviour
             mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
             lineRenderer.SetPosition(0, gameObject.transform.position);
             lineRenderer.SetPosition(1, new Vector3(mousePos.x, mousePos.y, gameObject.transform.position.z));
-
         }
-
-
 
     }
 
@@ -45,22 +35,14 @@ public class matchingGame : MonoBehaviour
     }
     void OnMouseUp()
     {
-        Debug.Log("k");
-
         isDrawing = false;
-        endPos.x = lineRenderer.GetPosition(1).x;
-        endPos.y = lineRenderer.GetPosition(1).y;
         if (endPoint.GetComponent<Collider2D>().bounds.ClosestPoint(lineRenderer.GetPosition(1)) == lineRenderer.GetPosition(1))
         {
-            Debug.Log("yess");
             lineRenderer.SetPosition(1, endPoint.transform.position);
-
         }
         else
         {
-            Debug.Log("no: "+ endPoint.GetComponent<Collider2D>().bounds );
             lineRenderer.SetPosition(1, gameObject.transform.position);
-
         }
     }
 }
